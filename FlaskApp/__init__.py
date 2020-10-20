5718a185a897d273145c663df8300b86f24929bb
Vincent Mader <vincent.mader@icloud.com>
2020-10-20 23:11:20 +0200

    continued on chat funtionality

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,8 +1,18 @@
+from datetime import datetime as dt
+
 import flask
-from flask import Flask, render_template, send_from_directory, request
+from flask import Flask, render_template, send_from_directory, request, jsonify
 
 
 app = Flask(__name__)
+
+MESSAGES = {
+    'messages': [
+        dict(timestamp=109878978, msg_content='Test 1', sender='customer'),
+        dict(timestamp=109923890, msg_content='Test 2', sender='bot'),
+        dict(timestamp=109872390, msg_content='Test 3', sender='bot'),
+    ]
+}
 
 
 @app.route('/')
@@ -13,10 +23,25 @@ def chatbot():
 @app.route('/<customerID>/<chatID>', methods=['POST'])
 def chatbot_post(customerID, chatID):
 
-    query = request.form
-    print(query)
+    # print(request.form)
+
+    timestamp = dt.now().timestamp()
+    msg_content = request.form
+    msg_content = [i for i in msg_content.keys()][0]  # TODO: this is shitty
+    sender = 'customer'
+
+    if msg_content:
+        MESSAGES['messages'].append(dict(
+            timestamp=timestamp, msg_content=msg_content, sender=sender
+        ))
 
     return render_template('index.html')
+
+
+@app.route('/messageList/', methods=['GET'])
+def messageList():
+    # return MESSAGES
+    return jsonify(MESSAGES)
 
 
 if __name__ == '__main__':
--- FlaskApp/static/js/chatbot.js
+++ FlaskApp/static/js/chatbot.js
@@ -1,4 +1,4 @@
-function postAjaxCall(url, data) {
+function httpPost(url, data) {
   // return a new promise.
   return new Promise(function (resolve, reject) {
     // do the usual XHR stuff
@@ -21,18 +21,92 @@ function postAjaxCall(url, data) {
     //same thing if i hardcode like //req.send("limit=2");
   });
 }
+// function httpGet(url) {
+//   var xmlHttp = new XMLHttpRequest();
+//   xmlHttp.open("GET", url, true); // false for synchronous request
+//   xmlHttp.send(null);
+//   return xmlHttp.responseText;
+// }
+function httpGet(theUrl, callback) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function () {
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+      callback(xmlHttp.responseText);
+  };
+  xmlHttp.open("GET", theUrl, true); // true for asynchronous
+  xmlHttp.send(null);
+}
 
-let customerID = 1;
-let chatID = 1;
+sendButtonOnClick = function () {
+  httpPost(
+    // "https://webhook.site/b501642c-11cd-4329-8973-6f7916f677f6",
+    "http://localhost:5001/" + customerID + "/" + chatID,
+    inputForm.value
+  );
+  inputForm.value = "";
+  httpGet("http://localhost:5001/messageList", callbackfunc);
+};
 
-const css = document.createElement("link");
 const chatButton = document.createElement("div");
 const chatWindow = document.createElement("div");
 const inputForm = document.createElement("input");
 const header = document.createElement("div");
 const sendButton = document.createElement("button");
+const messageList = document.createElement("div");
 
 chatButton.id = "chatButton";
+chatWindow.id = "chatWindow";
+sendButton.id = "sendButton";
+messageList.id = "messageList";
+
+document.body.appendChild(chatWindow);
+document.body.appendChild(chatButton);
+document.getElementById("chatWindow").appendChild(header);
+document.getElementById("chatWindow").appendChild(inputForm);
+document.getElementById("chatWindow").appendChild(sendButton);
+document.getElementById("chatWindow").appendChild(messageList);
+
+const callbackfunc = function (res) {
+  let foo = JSON.parse(res)["messages"];
+  // console.log(typeof foo);
+  // console.log(foo);
+  document.getElementById("messageList").outerHTML = "";
+  const messageList = document.createElement("div");
+  messageList.id = "messageList";
+  messageList.style.overflow = "hidden";
+  messageList.style.position = "absolute";
+  messageList.style.top = "40px";
+  messageList.style.padding = "5px";
+  document.getElementById("chatWindow").appendChild(messageList);
+  for (let idx = 0; idx < foo.length; idx++) {
+    msg = foo[idx];
+    console.log(msg);
+    // messages.push(foo[idx]);
+
+    const msgBubble = document.createElement("div");
+    msgBubble.id = "msgBubble";
+    msgBubble.style.width = "200px";
+    msgBubble.style.height = "100px";
+    msgBubble.style.border = "2px solid pink";
+    msgBubble.style.margin = "5px";
+    document.getElementById("messageList").appendChild(msgBubble);
+
+    if (msg["sender"] != "bot") {
+      msgBubble.style.marginLeft = "80px";
+    }
+
+    msgBubble.innerHTML =
+      '<p style="margin-left:20px; font-family: Arial">' +
+      msg["msg_content"] +
+      "</p>";
+  }
+};
+
+httpGet("http://localhost:5001/messageList", callbackfunc);
+
+let customerID = 1;
+let chatID = 1;
+
 chatButton.style.color = "red";
 chatButton.style.backgroundColor = "white";
 chatButton.style.width = "100px";
@@ -44,7 +118,6 @@ chatButton.style.visibility = "visible";
 chatButton.style.borderRadius = "50%";
 chatButton.style.border = "3px solid gray";
 
-chatWindow.id = "chatWindow";
 chatWindow.style.backgroundColor = "white";
 chatWindow.style.visibility = "hidden";
 chatWindow.style.width = "300px";
@@ -87,25 +160,10 @@ header.style.borderBottom = "1px solid gray";
 header.style.backgroundColor = "white";
 header.style.top = "0px";
 header.style.width = "100%";
-// header.style.paddingRight = "20px";
-// header.style.paddingLeft = "20px";
 header.style.height = "40px";
 header.innerHTML = '<p style="margin-left:20px; font-family: Arial">Chat</p>';
 
-// css.href = "localhost/static/css/style.css";
-// css.type = "text/css";
-// css.rel = "stylesheet";
-
-sendButtonOnClick = function () {
-  postAjaxCall(
-    // "https://webhook.site/b501642c-11cd-4329-8973-6f7916f677f6",
-    "http://localhost:5001/" + customerID + "/" + chatID,
-    inputForm.value
-  );
-  inputForm.value = "";
-};
 sendButton.type = "button";
-sendButton.id = "sendButton";
 sendButton.value = "Send";
 sendButton.style.width = "100%";
 // sendButton.style.margin = "0px";
@@ -117,11 +175,3 @@ sendButton.style.position = "absolute";
 // sendButton.style.right = "10px";
 sendButton.style.bottom = "0px";
 sendButton.onclick = sendButtonOnClick;
-
-// document.head.append(css);
-document.body.appendChild(chatWindow);
-document.body.appendChild(chatButton);
-document.getElementById("chatWindow").appendChild(header);
-document.getElementById("chatWindow").appendChild(inputForm);
-document.getElementById("chatWindow").appendChild(sendButton);
-// document.body.appendChild(jquery);
